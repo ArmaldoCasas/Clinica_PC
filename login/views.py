cef70b14bafdbd38e619c51c85fd5656650e30a2
Armaldo <armando.v.mohr@gmail.com>
2025-09-12 18:28:48 -0300

    Login Hecho falta que lleve a registrar

--- login/views.py
+++ login/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+def login_view(request):
+    error = None
+    if request.method == "POST":
+        usuario = request.POST.get('usuario')
+        password = request.POST.get('password')
+        if usuario == "inacap" and password == "clinica2025":
+            request.session['autenticado'] = True
+            return redirect('/recepcion/registrar.html')
+        else:
+            error = "Usuario o clave incorrectos intentelo otra vez."
+            
+            
+    return render(request, 'Clinica_PC/login.html', {'error': error})
 
 # Create your views here.
